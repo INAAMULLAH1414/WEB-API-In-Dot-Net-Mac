ae31dcc9fba1d8184e81975fb87807c921008421
INAAMULLAH1414 <inamullah1414@gmail.com>
2023-12-22 09:37:06 -0600

    Added Models

---
+++ Models/RpgClass.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WEB_API_In_Dot_Net_Mac.Models
+{
+     public enum RpgClass
+    {
+        Knight = 1,
+        Mage = 2,
+        Cleric = 3
+    }
+}

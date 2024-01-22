17c5ea5954d325bb6900643320d970d9f59784b9
INAAMULLAH1414 <inamullah1414@gmail.com>
2024-01-22 11:54:42 -0600

    User::Implemented User Registration

--- Data/AuthRepository.cs
+++ Data/AuthRepository.cs
@@ -7,19 +7,44 @@ namespace WEB_API_In_Dot_Net_Mac.Data
 {
     public class AuthRepository : IAuthRepository
     {
+        private readonly DataContext _dataContext;
+
+        public AuthRepository(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
         public Task<ServiceResponse<string>> Login(string username, string password)
         {
             throw new NotImplementedException();
         }
 
-        public Task<ServiceResponse<int>> Register(User user, string password)
+        public async Task<ServiceResponse<int>> Register(User user, string password)
         {
-            throw new NotImplementedException();
+            CreatePasswordHash(password, out byte[] passwordHash, out byte[] passwordSalt);
+
+            user.PasswordHash = passwordHash;
+            user.PasswordSalt = passwordSalt;
+            
+           _dataContext.Users.Add(user);
+           await _dataContext.SaveChangesAsync();
+
+           var response = new ServiceResponse<int>();
+           response.Data = user.Id;
+           return response;
         }
 
         public Task<bool> UserExists(string username)
         {
             throw new NotImplementedException();
+        }
+
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using(var hmac = new System.Security.Cryptography.HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
         }
     }
 }

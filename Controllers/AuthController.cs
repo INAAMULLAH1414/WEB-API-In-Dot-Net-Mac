739ab49060aa47cbdd2d3447a21cefee54227050
INAAMULLAH1414 <inamullah1414@gmail.com>
2024-01-27 13:13:33 -0600

    Added Auth Controller

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WEB_API_In_Dot_Net_Mac.Dtos.User;
+
+namespace WEB_API_In_Dot_Net_Mac.Controllers
+{
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthRepository _authRepo;
+        public AuthController(IAuthRepository authRepo)
+        {
+            _authRepo = authRepo;
+        }
+
+        [HttpPost("Register")]
+        public async Task<ActionResult<ServiceResponse<int>>> Register(UserRegisterDto request)
+        {
+            var response = await _authRepo.Register(
+                new User { Username = request.UserName}, request.Password
+            );
+
+            if(response.Success){
+                return Ok(response);
+            } 
+            else
+            {
+                return BadRequest(response);
+            }
+        }
+    }
+}

22e7457cf3f8dc633dbae1df636f4705269e804c
INAAMULLAH1414 <inamullah1414@gmail.com>
2024-02-21 00:20:39 +0500

    Added Start Fighting implementation

---
+++ Dtos/Fight/FightResultDto.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WEB_API_In_Dot_Net_Mac.Dtos.Fight
+{
+    public class FightResultDto
+    {
+        public List<string> Log { get; set; } = new List<string>();
+    }
+}
--- Services/FightService/IFightService.cs
+++ Services/FightService/IFightService.cs
@@ -10,5 +10,6 @@ namespace WEB_API_In_Dot_Net_Mac.Services.FightService
     {
         Task<ServiceResponse<AttackResultDto>> WeaponAttack(WeaponAttackDto request);
         Task<ServiceResponse<AttackResultDto>> SkillAttack(SkillAttackDto request);
+        Task<ServiceResponse<FightResultDto>> Fight(FightRequestDto request);
     }
 }
